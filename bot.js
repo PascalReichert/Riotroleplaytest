e00cf517ea845b84c63a86eff07f7775073763ec
PascalReichert <32631847+PascalReichert@users.noreply.github.com>
2017-12-11 14:54:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,60 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+var Discord = require('discord.js');
+var economy = require('discord-eco');
+var bot = new Discord.Client();
+var fs = require('fs');
+var moment = require('moment');
 
-client.on('ready', () => {
+
+var commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
+
+bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.channel.send('pong');
-  	}
+bot.on('message', message => {
+    
+  var commandsList = fs.readFileSync('Storage/commands.txt', 'utf8');
+
+    var sender = message.author; //the person who sent the message
+    var msg = message.content.toUpperCase(); //takes message and makes it all uppercase
+    var prefix = '!' //the text before commands
+    var cont = message.content.slice(prefix.length).split(" ");
+    var args = cont.slice(1);
+    if (sender.id === '389333933671579668'){
+      return;
+    }
+    
+    if (msg === prefix + 'HELP' || msg === prefix + 'COMMANDS')
+    {
+      message.channel.send(commandsList)
+    }
+    
+    if(msg === prefix + 'PING')
+    {
+        message.channel.send('Pong! Ping = **' + bot.ping + '**')
+    }
+    
+    if (msg === prefix + 'BALANCE'){
+
+      economy.fetchBalance(message.author.id).then((i) => {
+        message.channel.send({embed:{
+          title: "Bank",
+          color: 0xFFDC00,
+          fields:[{
+            name: "Account Holder",
+            value: message.author.username,
+            inline:true
+          },
+          {
+            name: "Account Balance",
+            value: i.money,
+            inline: true
+          }
+        ]
+        }})
+      })
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
